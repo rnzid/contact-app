44c8b99cbcafc98b9790a89fa5dbe905e9962b73
rnzid <ranjitdangol13@gmail.com>
2022-07-15 14:09:30 +0545

    Props use

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,7 @@ const Header = () => {
         <div className="ui fixed menu">
             <div className="ui container center">
                 <h2>Contact manager</h2>
-
             </div>
-
         </div>
   )
 }
